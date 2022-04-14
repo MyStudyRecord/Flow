70258708ac82ccacd88934042aed6fcb80b7b005
ParkSangSun <appleasz1028102@gmail.com>
2022-04-14 09:40:15 +0900

    동기와 비동기 차이 비교

---
+++ app/src/main/java/com/pss/flow/Helper.kt
@@ -0,0 +1,3 @@
+package com.pss.flow
+
+val userList = listOf("User1", "User2", "User3", "User4", "User5")
--- app/src/main/java/com/pss/flow/MainActivity.kt
+++ app/src/main/java/com/pss/flow/MainActivity.kt
@@ -2,10 +2,40 @@ package com.pss.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        printUser()
+        Log.d(TAG, "print user completed")
+
+        //suspend
+        CoroutineScope(Dispatchers.IO).launch {
+            printUserList()
+        }
+        Log.d(TAG, "print userList completed for suspend fun")
+
+    }
+
+    private fun printUser(){
+        userList.forEach{user ->
+            Log.d(TAG, user)
+        }
+    }
+
+    private suspend fun printUserList(){
+        userList.forEach { user ->
+            Log.d(TAG,user)
+        }
     }
 }
